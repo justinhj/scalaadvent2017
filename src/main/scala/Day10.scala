e05f7c97c69cfd7b9d41823cedfc1ed2744baea6
Justin Heyes-Jones <justinhj.iggca@gmail.com>
2017-12-22 10:16:35 -0800

    Day 10 done

--- src/main/scala/Day10.scala
+++ src/main/scala/Day10.scala
@@ -52,10 +52,63 @@ In this example, the first two numbers in the list end up being 3 and 4; to chec
 However, you should instead use the standard list size of 256 (with values 0 to 255) and the sequence of lengths in
 your puzzle input. Once this process is complete, what is the result of multiplying the first two numbers in the list?
 
+Part two
+
+--- Part Two ---
+
+The logic you've constructed forms a single round of the Knot Hash algorithm; running the full thing requires many of
+these rounds. Some input and output processing is also required.
+
+First, from now on, your input should be taken not as a list of numbers, but as a string of bytes instead. Unless
+otherwise specified, convert characters to bytes using their ASCII codes. This will allow you to handle arbitrary
+ASCII strings, and it also ensures that your input lengths are never larger than 255. For example, if you are
+given 1,2,3, you should convert it to the ASCII codes for each character: 49,44,50,44,51.
+
+Once you have determined the sequence of lengths to use, add the following lengths to the end of the
+sequence: 17, 31, 73, 47, 23. For example, if you are given 1,2,3, your final sequence of lengths
+should be 49,44,50,44,51,17,31,73,47,23 (the ASCII codes from the input string combined with the standard length
+suffix values).
+
+Second, instead of merely running one round like you did above, run a total of 64 rounds, using the same length
+sequence in each round. The current position and skip size should be preserved between rounds. For example, if
+the previous example was your first round, you would start your second round with the same length
+sequence (3, 4, 1, 5, 17, 31, 73, 47, 23, now assuming they came from ASCII codes and include the suffix),
+but start with the previous round's current position (4) and skip size (4).
+
+Once the rounds are complete, you will be left with the numbers from 0 to 255 in some order, called the sparse hash.
+Your next task is to reduce these to a list of only 16 numbers called the dense hash. To do this, use numeric bitwise
+XOR to combine each consecutive block of 16 numbers in the sparse hash (there are 16 such blocks in a list of
+256 numbers). So, the first element in the dense hash is the first sixteen elements of the sparse hash XOR'd together,
+the second element in the dense hash is the second sixteen elements of the sparse hash XOR'd together, etc.
+
+For example, if the first sixteen elements of your sparse hash are as shown below, and the XOR operator is ^, you
+would calculate the first output number like this:
+
+65 ^ 27 ^ 9 ^ 1 ^ 4 ^ 3 ^ 40 ^ 50 ^ 91 ^ 7 ^ 6 ^ 0 ^ 2 ^ 5 ^ 68 ^ 22 = 64
+Perform this operation on each of the sixteen blocks of sixteen numbers in your sparse hash to determine the sixteen
+numbers in your dense hash.
+
+Finally, the standard way to represent a Knot Hash is as a single hexadecimal string; the final output is the dense hash
+in hexadecimal notation. Because each number in your dense hash will be between 0 and 255 (inclusive), always represent
+each number as two hexadecimal digits (including a leading zero as necessary). So, if your first three numbers are
+64, 7, 255, they correspond to the hexadecimal numbers 40, 07, ff, and so the first six characters of the hash would be
+4007ff. Because every Knot Hash is sixteen such numbers, the hexadecimal representation is always 32 hexadecimal digits
+(0-f) long.
+
+Here are some example hashes:
+
+The empty string becomes a2582a3a0e66e6e86e3812dcb672a272.
+AoC 2017 becomes 33efeb34ea91902bb2f59c9920caa6cd.
+1,2,3 becomes 3efbe78a8d82f29979031a4aa0b16a9d.
+1,2,4 becomes 63960835bcdc130f0b66d7ff4f6a5a8e.
+Treating your puzzle input as a string of ASCII characters, what is the Knot Hash of your puzzle input? Ignore any leading or trailing whitespace you might encounter.
+
 
    */
 
-  val taskInput = "31,2,85,1,80,109,35,63,98,255,0,13,105,254,128,33"
+  val HASH_RUNS = 64 // how many times to iterate the hash
+
+  val   taskInput = "31,2,85,1,80,109,35,63,98,255,0,13,105,254,128,33"
 
   // Helper to copy a vector over a vector but instead of lengthening it for overlap we must wrap around
 
@@ -99,7 +152,73 @@ your puzzle input. Once this process is complete, what is the result of multiply
     finalRing(0) * finalRing(1)
   }
 
+  // For step 2 we need to keep track of the current position and skip values
+  // so I've rewritten the findHash to allow that
+  // It also returns the new input at each stage instead of the hash from step 1
+  // to make things easier to read I also defined a case class that acts as both the
+  // state in the foldLeft and the return value
+
+  case class HashState(curPos: Int, skip: Int, ring: Vector[Int]) {
+    val ringSize = ring.size
+  }
+
+  def findHash2(hashState: HashState, steps: List[Int]) : HashState = {
+
+    steps.foldLeft(hashState) {
+      case (curstate, step) =>
+        val newInput = reverseSection(curstate.curPos, step, curstate.ring)
+        HashState((curstate.curPos + step + curstate.skip) % curstate.ringSize, curstate.skip + 1, newInput)
+    }
+
+  }
+
+  def iterateHash(ringSize: Int, steps: List[Int], times: Int) : HashState = {
+
+    val ring = (0 until ringSize).toVector
+
+    val startState = HashState(0, 0, ring)
+
+    (1 to times).foldLeft(startState) {
+      case (curstate, count) =>
+        findHash2(curstate, steps)
+    }
+  }
+
+  def xorGroup(group: Vector[Int]) : Int = group.reduce(_ ^ _)
+
+  def calculateDenseHash(ring: Vector[Int]): Vector[Int] = {
+
+    // divide into groups of 16 and XOR the members of each group
+
+    val groups = ring.grouped(16).toList
+
+    groups.map(xorGroup).toVector
+  }
+
+  // Returns the ascii value of each character and adds the provided suffix
+  def stepTwoInputConverter(in: String): List[Int] = in.map(_.charValue.toInt).toList ++ List(17, 31, 73, 47, 23)
+
+  // Convert an int to a 2 digit hex making sure to have a leading zero
+
+  def toHex(in: Int): String = {
+    val s = in.toHexString
+    if(s.size == 1) "0" + s
+    else s
+  }
+
+  def calcHash(input: String) : String = {
+    val inputConverted = stepTwoInputConverter(input)
+
+    val iteratedHash = iterateHash(256, inputConverted, HASH_RUNS)
+    val denseHash = calculateDenseHash(iteratedHash.ring)
+
+    denseHash.map(toHex).mkString
+
+  }
+
   def main(args : Array[String]) = {
+
+    // Step one
 
     val testInputs = "3 4 1 5".split(" ").toList.map(_.toInt)
 
@@ -112,6 +231,32 @@ your puzzle input. Once this process is complete, what is the result of multiply
     val taskHash = findHash(256, taskInputs)
 
     println(s"task input gave hash $taskHash") // gave 6952 which is correct
+
+    // Step two
+
+    // Input is now ascii values rather than convert to int
+
+    val testInputs2 = stepTwoInputConverter("3,4,1,5")
+
+    assert(testInputs2 == List(51,44,52,44,49,44,53,17,31,73,47,23))
+
+    val test2 = stepTwoInputConverter("1,2,3")
+
+    val iteratedHash = iterateHash(256, testInputs2, HASH_RUNS)
+
+    val denseHash = calculateDenseHash(iteratedHash.ring)
+
+    assert(xorGroup(Vector(65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22)) == 64)
+
+    assert(calcHash("") == "a2582a3a0e66e6e86e3812dcb672a272")
+    assert(calcHash("AoC 2017") == "33efeb34ea91902bb2f59c9920caa6cd")
+    assert(calcHash("1,2,3") == "3efbe78a8d82f29979031a4aa0b16a9d")
+    assert(calcHash("1,2,4") == "63960835bcdc130f0b66d7ff4f6a5a8e")
+
+    val step2 = calcHash(taskInput)
+
+    println(s"Step 2 result is $step2")
+
 
   }
 
