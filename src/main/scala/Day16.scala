9573db8be342e8b2f52480b847eb36789a92499b
Justin Heyes-Jones <justinhj.iggca@gmail.com>
2018-03-15 17:37:22 -0700

    Day 16 almost done

--- src/main/scala/Day16.scala
+++ src/main/scala/Day16.scala
@@ -4,6 +4,7 @@ import cats.effect.IO
 import fs2.{Chunk, Pure, Stream, text}
 import cats.instances.all
 import cats.implicits._
+import com.sun.org.omg.CORBA.ExcDescriptionSeqHelper
 
 import scala.util.Try
 
@@ -37,14 +38,6 @@ standing after their dance?
 
 object Day16 {
 
-  // TODO better to use Array or Vector
-
-  val a1 = Array[Int](1,2,3)
-
-
-
-  def dance(input: String) = ???
-
 //  Spin, written sX, makes X programs move from the end to the front, but maintain their order otherwise. (For
 //    example, s3 on abcde produces cdeab).
 //    Exchange, written xA/B, makes the programs at positions A and B swap places.
@@ -52,7 +45,9 @@ object Day16 {
 
   trait DanceMove
 
-  case class Spin(len: Int) extends DanceMove {
+  case class Spin(len: Int) extends DanceMove
+
+  object Spin {
     def fromString(s: String) : Option[DanceMove] = {
 
       val spinPattern = """s([0-9]+)""".r
@@ -66,7 +61,9 @@ object Day16 {
     }
   }
 
-  case class Exchange(a: Int, b: Int) extends DanceMove {
+  case class Exchange(a: Int, b: Int) extends DanceMove
+
+  object Exchange {
     def fromString(s: String) : Option[DanceMove] = {
 
       val exchangePattern = """x([0-9]+)/([0-9]+)""".r
@@ -80,7 +77,9 @@ object Day16 {
     }
   }
 
-  case class Partner(a: Char, b : Char) extends DanceMove {
+  case class Partner(a: Char, b : Char) extends DanceMove
+
+  object Partner {
     def fromString(s: String) : Option[DanceMove] = {
 
       val partnerPattern = """p([a-p])/([a-p])""".r
@@ -95,37 +94,86 @@ object Day16 {
   }
 
   object DanceMove {
-    def parse(s: String) = {
 
-      val tryParse = ()
+    // Figure out which dance move by the first letter then use the appropriate parser
+    // Note we will throw an exception here if the DanceMove cannot be parsed
+    // through the use of .get on the option
+    // We will also throw an exception if the first letter is not a valid dance move
+    // In a real system you'd want to handle the errors better, for example use
+    // an Either and return the reason the parse failed to the caller
+
+    def fromString(s: String) : DanceMove = {
+
+      s(0) match {
+        case 'p' => Partner.fromString(s).get
+        case 's' => Spin.fromString(s).get
+        case 'x' => Exchange.fromString(s).get
+      }
 
     }
+
+    // Executes the moves
+    def doMove(dm : DanceMove, dancers: Vector[Char]) : Vector[Char] = {
+
+      dm match {
+        // Spin, written sX, makes X programs move from the end to the front, but maintain their order otherwise. (For
+        // example, s3 on abcde produces cdeab).
+        case Spin(len) =>
+          val spinCount = dancers.size - len
+          dancers.drop(spinCount) ++ dancers.take(spinCount)
+
+        // Exchange, written xA/B, makes the programs at positions A and B swap places.
+        case Exchange(a,b) =>
+          val elementAtA = dancers(a)
+          val elementAtB = dancers(b)
+          dancers.updated(a, elementAtB).updated(b, elementAtA)
+
+        //Partner, written pA/B, makes the programs named A and B swap places.
+        case Partner(a,b) =>
+          val positionOfA = dancers.indexOf(a)
+          val positionOfB = dancers.indexOf(b)
+
+          doMove(Exchange(positionOfA, positionOfB), dancers)
+
+      }
+
+    }
+
   }
 
   def main(args: Array[String]) : Unit = {
 
     val sample = "s1,x3/4,pe/b"
 
-    // TODO FS2 Stream
+    // read the input
+    // delimit by comma
+    // turn each string into a command
+    // apply the commands to the start input
 
     val input: Stream[Pure, String] = Stream.emit(sample)
 
     val splitOnCommas: Stream[Pure, String] = input.repartition{ s => Chunk.array(s.split(","))}
 
-    val commands = splitOnCommas.map{DanceMove.fromString(_)}
+    val commands: Stream[Pure, DanceMove] = splitOnCommas.map{DanceMove.fromString(_)}
 
+    val endOfTheWorld = commands.toList
 
-    // create the three types of commands
+    // test moves
 
-    // read the input
-    // delimit by comma
-    // turn each string into a command
-    // fold apply the commands to the start input
+    val spinTest = DanceMove.doMove(Spin(1), Vector('a','b','c','d','e'))
+    assert(spinTest == Vector('e','a','b','c','d'))
 
+    val exchangeTest = DanceMove.doMove(Exchange(3,4), Vector('e','a','b','c','d'))
+    assert(exchangeTest == Vector('e','a','b','d','c'))
 
+    val partnerTest = DanceMove.doMove(Partner('e','b'), Vector('e','a','b','c','d'))
+    assert(partnerTest == Vector('b','a','e','c','d'))
 
+    val step1Dancers = Vector('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p')
 
-    println("day 16")
+    println("day 16 answer for step 1")
+
+    
 
   }
 
