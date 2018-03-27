846be14af68efdcb4ea8b0fa5ce9d2eadb7477f1
Justin Heyes-Jones <justinhj@gmail.com>
2018-03-26 23:11:15 -0700

    get started on day 18 and write parser

---
+++ src/main/scala/Day18.scala
@@ -0,0 +1,262 @@
+import fs2.{Chunk, Pure}
+
+import scala.util.Try
+
+object Day18 {
+
+  val sampleInstructions = """set a 1
+                             |add a 2
+                             |mul a a
+                             |mod a 5
+                             |snd a
+                             |set a 0
+                             |rcv a
+                             |jgz a -1
+                             |set a 1
+                             |jgz a -2""".stripMargin
+
+  trait Instruction
+
+  type Reg = Char
+
+  case class Snd(r: Reg) extends Instruction
+
+  object Snd {
+    def fromString(s: String) : Option[Snd] = {
+      val m1 = "snd ([a-z])".r
+      Try {
+        val m1(a) = s
+        Snd(a.head)
+      }.toOption
+    }
+  }
+
+  case class SetV(r: Reg, value: Int) extends Instruction
+
+  object SetV {
+    def fromString(s: String) : Option[SetV] = {
+      val m1 = "set ([a-z]) ([-]*[0-9]+)".r
+      Try {
+        val m1(a,b) = s
+        SetV(a.head, b.toInt)
+      }.toOption
+    }
+  }
+
+  case class SetR(r: Reg, value: Reg) extends Instruction
+
+  object SetR {
+    def fromString(s: String) : Option[SetR] = {
+      val m1 = "set ([a-z]) ([a-z])".r
+      Try {
+        val m1(a,b) = s
+        SetR(a.head, b.head)
+      }.toOption
+    }
+  }
+
+  case class AddV(r: Reg, value: Int) extends Instruction
+
+  object AddV {
+    def fromString(s: String) : Option[AddV] = {
+      val m1 = "add ([a-z]) ([-]*[0-9]+)".r
+      Try {
+        val m1(a,b) = s
+        AddV(a.head, b.toInt)
+      }.toOption
+    }
+  }
+
+  case class AddR(r: Reg, value: Reg) extends Instruction
+
+  object AddR {
+    def fromString(s: String) : Option[AddR] = {
+      val m1 = "add ([a-z]) ([a-z])".r
+      Try {
+        val m1(a,b) = s
+        AddR(a.head, b.head)
+      }.toOption
+    }
+  }
+
+  case class MulV(r1: Reg, value: Int) extends Instruction
+
+  object MulV {
+    def fromString(s: String) : Option[MulV] = {
+      val m1 = "mul ([a-z]) ([-]*[0-9]+)".r
+      Try {
+        val m1(a,b) = s
+        MulV(a.head, b.toInt)
+      }.toOption
+    }
+  }
+
+  case class MulR(r1: Reg, r2: Reg) extends Instruction
+
+  object MulR {
+    def fromString(s: String) : Option[MulR] = {
+      val m1 = "mul ([a-z]) ([a-z])".r
+      Try {
+        val m1(a,b) = s
+        MulR(a.head, b.head)
+      }.toOption
+    }
+  }
+
+  case class ModV(r1: Reg, value: Int) extends Instruction
+
+  object ModV {
+    def fromString(s: String) : Option[ModV] = {
+      val m1 = "mod ([a-z]) ([-]*[0-9]+)".r
+      Try {
+        val m1(a,b) = s
+        ModV(a.head, b.toInt)
+      }.toOption
+    }
+  }
+
+  case class ModR(r1: Reg, r2: Reg) extends Instruction
+
+  object ModR {
+    def fromString(s: String) : Option[ModR] = {
+      val m1 = "mul ([a-z]) ([a-z])".r
+      Try {
+        val m1(a,b) = s
+        ModR(a.head, b.head)
+      }.toOption
+    }
+  }
+
+  case class Rcv(r1 : Reg) extends Instruction
+
+  object Rcv {
+    def fromString(s: String) : Option[Rcv] = {
+      val m1 = "rcv ([a-z])".r
+      Try {
+        val m1(a) = s
+        Rcv(a.head)
+      }.toOption
+    }
+  }
+
+  case class JgzR(r1 : Reg, r2: Reg) extends Instruction
+
+  object JgzR {
+    def fromString(s: String) : Option[JgzR] = {
+      val m1 = "jgz ([a-z]) ([a-z])".r
+      Try {
+        val m1(a,b) = s
+        JgzR(a.head, b.head)
+      }.toOption
+    }
+  }
+
+  case class JgzV(r1 : Reg, value: Int) extends Instruction
+
+  object JgzV {
+    def fromString(s: String) : Option[JgzV] = {
+      val m1 = "jgz ([a-z]) ([-]*[0-9]+)".r
+      Try {
+        val m1(a,b) = s
+        JgzV(a.head, b.toInt)
+      }.toOption
+    }
+  }
+
+  object Instruction {
+    def fromString(s: String) : Instruction = {
+
+      val instruction = List(Snd.fromString(s),
+        MulR.fromString(s),
+        MulV.fromString(s),
+        AddR.fromString(s),
+        AddV.fromString(s),
+        SetR.fromString(s),
+        SetV.fromString(s),
+        ModR.fromString(s),
+        ModV.fromString(s),
+        Rcv.fromString(s),
+        JgzR.fromString(s),
+        JgzV.fromString(s)
+      ).flatten
+
+      instruction.head
+    }
+
+  }
+
+  def readInstructionsFromString(s: String) : Vector[Instruction] =
+    s.lines.toVector.map{Instruction.fromString}
+
+  def main(args: Array[String]) : Unit = {
+
+
+    println(s"snd ${Snd.fromString("snd a")}")
+    println(s"set ${SetV.fromString("set c -12")}")
+    println(s"set ${SetV.fromString("set z 42")}")
+
+    println(readInstructionsFromString(sampleInstructions))
+
+  }
+
+
+
+
+}
+
+/*
+
+--- Day 18: Duet ---
+You discover a tablet containing some strange assembly code labeled simply "Duet". Rather than bother the sound card
+with it, you decide to run the code yourself. Unfortunately, you don't see any documentation, so you're left to figure
+out what the instructions mean on your own.
+
+It seems like the assembly is meant to operate on a set of registers that are each named with a single letter and that
+can each hold a single integer. You suppose each register should start with a value of 0.
+
+There aren't that many instructions, so it shouldn't be hard to figure out what they do. Here's what you determine:
+
+snd X plays a sound with a frequency equal to the value of X.
+set X Y sets register X to the value of Y.
+add X Y increases register X by the value of Y.
+mul X Y sets register X to the result of multiplying the value contained in register X by the value of Y.
+mod X Y sets register X to the remainder of dividing the value contained in register X by the value of Y (that is, it
+ sets X to the result of X modulo Y).
+rcv X recovers the frequency of the last sound played, but only when the value of X is not zero. (If it is zero, the
+command does nothing.)
+jgz X Y jumps with an offset of the value of Y, but only if the value of X is greater than zero. (An offset of 2 skips
+the next instruction, an offset of -1 jumps to the previous instruction, and so on.)
+Many of the instructions can take either a register (a single letter) or a number. The value of a register is the
+ integer it contains; the value of a number is that number.
+
+After each jump instruction, the program continues with the instruction to which the jump jumped. After any other
+instruction, the program continues with the next instruction. Continuing (or jumping) off either end of the program
+terminates it.
+
+For example:
+
+set a 1
+add a 2
+mul a a
+mod a 5
+snd a
+set a 0
+rcv a
+jgz a -1
+set a 1
+jgz a -2
+The first four instructions set a to 1, add 2 to it, square it, and then set it to itself modulo 5, resulting in a
+ value of 4.
+Then, a sound with frequency 4 (the value of a) is played.
+After that, a is set to 0, causing the subsequent rcv and jgz instructions to both be skipped (rcv because a is 0,
+ and jgz because a is not greater than 0).
+Finally, a is set to 1, causing the next jgz instruction to activate, jumping back two instructions to another jump,
+which jumps again to the rcv, which ultimately triggers the recover operation.
+At the time the recover operation is executed, the frequency of the last sound played is 4.
+
+What is the value of the recovered frequency (the value of the most recently played sound) the first time a rcv
+instruction is executed with a non-zero value?
+
+
+
+ */
