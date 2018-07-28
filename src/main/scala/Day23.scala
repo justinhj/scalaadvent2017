3e0bdf41230136aabde2ac3d044f8d9dada8f9db
Justin Heyes-Jones <justinhj.iggca@gmail.com>
2018-07-28 09:38:50 +0800

    start day 22

--- src/main/scala/Day23.scala
+++ src/main/scala/Day23.scala
@@ -11,9 +11,15 @@ object Day23 {
 
   case class Machine(regs : Map[Reg, Long] = Map.empty,
                      mulCount : Long = 0L,
-                     nextInstructionOffset : Int = 1) {
+                     nextInstructionOffset : Int = 1,
+                     defaultAValue : Int = 0) {
 
-    def get(reg: Reg): Long = regs.getOrElse(reg, 0)
+    def get(reg: Reg): Long = {
+      if(reg == 'a')
+        regs.getOrElse(reg, defaultAValue)
+      else
+        regs.getOrElse(reg, 0)
+    }
 
     def set(reg: Reg, value: Long) : Machine = {
 
@@ -233,13 +239,19 @@ object Day23 {
 
   def main(args: Array[String]) : Unit = {
 
-    val step1Input = Source.fromResource("input23.txt").mkString
+    val step1Input = Source.fromResource("input23optimized.txt").mkString
 
     val step1Program = readInstructionsFromString(step1Input)
 
-    val s1 = execute(Machine(), step1Program)
+//    val s1 = execute(Machine(), step1Program)
 
-    println(s"step1 mul was called $s1 times")
+  //  println(s"step1 mul was called $s1 times")
+
+    // part two
+
+    val s2 = execute(Machine(defaultAValue = 1), step1Program)
+
+    //println(s"step1 mul was called $s1 times")
 
   }
 
@@ -248,17 +260,38 @@ object Day23 {
 /*
 
 --- Day 23: Coprocessor Conflagration ---
-You decide to head directly to the CPU and fix the printer from there. As you get close, you find an experimental coprocessor doing so much work that the local programs are afraid it will halt and catch fire. This would cause serious issues for the rest of the computer, so you head in and see what you can do.
+You decide to head directly to the CPU and fix the printer from there. As you get close, you find an experimental coprocessor
+doing so much work that the local programs are afraid it will halt and catch fire. This would cause serious issues for the
+ rest of the computer, so you head in and see what you can do.
 
-The code it's running seems to be a variant of the kind you saw recently on that tablet. The general functionality seems very similar, but some of the instructions are different:
+The code it's running seems to be a variant of the kind you saw recently on that tablet. The general functionality seems
+very similar, but some of the instructions are different:
 
 set X Y sets register X to the value of Y.
 sub X Y decreases register X by the value of Y.
 mul X Y sets register X to the result of multiplying the value contained in register X by the value of Y.
-jnz X Y jumps with an offset of the value of Y, but only if the value of X is not zero. (An offset of 2 skips the next instruction, an offset of -1 jumps to the previous instruction, and so on.)
+jnz X Y jumps with an offset of the value of Y, but only if the value of X is not zero. (An offset of 2 skips the next
+instruction, an offset of -1 jumps to the previous instruction, and so on.)
 Only the instructions listed above are used. The eight registers here, named a through h, all start at 0.
 
-The coprocessor is currently set to some kind of debug mode, which allows for testing, but prevents it from doing any meaningful work.
+The coprocessor is currently set to some kind of debug mode, which allows for testing, but prevents it from doing any
+meaningful work.
 
 If you run the program (your puzzle input), how many times is the mul instruction invoked?
+
+--- Part Two ---
+Now, it's time to fix the problem.
+
+The debug mode switch is wired directly to register a. You flip the switch, which makes register a now start at 1 when
+the program is executed.
+
+Immediately, the coprocessor begins to overheat. Whoever wrote this program obviously didn't choose a very efficient
+implementation. You'll need to optimize the program if it has any hope of completing before Santa needs that printer working.
+
+
+The coprocessor's ultimate goal is to determine the final value left in register h once the program completes.
+ Technically, if it had that... it wouldn't even need to run the program.
+
+After setting register a to 1, if the program were to run to completion, what value would be left in register h?
+
  */
